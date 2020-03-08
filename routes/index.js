fb830f019db033bd8c6bef9172f4970d97822989
Ajdin Komic <ajdin.komic12@gmail.com>
2020-03-08 23:18:32 +0100

    user routes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,59 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Board Shop - Home' });
+  res.render('index', {
+    title: 'Board Shop - Home'
+  });
+});
+
+/* GET /register */
+router.get('/register', (req, res, next) => {
+  res.send('GET /register');
+});
+
+/* POST /register */
+router.post('/register', (req, res, next) => {
+  res.send('POST /register');
+});
+
+/* GET /login */
+router.get('/login', (req, res, next) => {
+  res.send('GET /login');
+});
+
+/* POST /login */
+router.post('/login', (req, res, next) => {
+  res.send('POST /login');
+});
+
+/* GET /profile */
+router.get('/profile', (req, res, next) => {
+  res.send('GET /profile');
+});
+
+/* PUT /profile/:user_id */
+router.put('/profile/:user_id', (req, res, next) => {
+  res.send('PUT /profile/:user_id');
+});
+
+/* GET /forgot-password */
+router.get('/forgot', (req, res, next) => {
+  res.send('GET /forgot');
+});
+
+/* PUT /forgot-password */
+router.put('/forgot', (req, res, next) => {
+  res.send('PUT /forgot');
+});
+
+/* GET /reset-password */
+router.GET('/reset/:token', (req, res, next) => {
+  res.send('GET /reset');
+});
+
+/* PUT /reset-password */
+router.put('/reset/:token', (req, res, next) => {
+  res.send('PUT /reset');
 });
 
 module.exports = router;
