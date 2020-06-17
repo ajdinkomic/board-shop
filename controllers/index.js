23e64bbe92c0b54c96c0ac991100e8f70b5b861a
Ajdin Komic <ajdin.komic12@gmail.com>
2020-06-18 00:18:11 +0200

    update controllers/index

--- controllers/index.js
+++ controllers/index.js
@@ -3,11 +3,13 @@ const User = require("../models/User");
 module.exports = {
   // postRegister is a method on object
   postRegister(req, res, next) {
+    
     const newUser = new User({
       username: req.body.username,
       email: req.body.email,
       image: req.body.image
     });
+
     User.register(newUser, req.body.password, err => {
       if (err) {
         console.log('error while user register: ', err);
@@ -15,9 +17,7 @@ module.exports = {
       }
 
       console.log('user registered');
-
       res.redirect('/')
-
     });
   }
 }
